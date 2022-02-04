e991fa99f01d26828efbf021af1d10de0075098d
zsquaredz <zhaozheng1997@hotmail.com>
2022-02-04 01:22:58 +0000

    ingore svd for now

--- analysis.py
+++ analysis.py
@@ -18,34 +18,36 @@ def SVCCA(file1, file2):
     # print(acts1.shape) # need to be (number of neurons, number of test data points)
     # print(acts2.shape)
 
-    # Mean subtract activations
-    cacts1 = acts1 - np.mean(acts1, axis=1, keepdims=True)
-    cacts2 = acts2 - np.mean(acts2, axis=1, keepdims=True)
+    # # Mean subtract activations
+    # cacts1 = acts1 - np.mean(acts1, axis=1, keepdims=True)
+    # cacts2 = acts2 - np.mean(acts2, axis=1, keepdims=True)
 
-    print('starting to perform SVD')
-    # Perform SVD
-    U1, s1, V1 = np.linalg.svd(cacts1, full_matrices=False)
-    U2, s2, V2 = np.linalg.svd(cacts2, full_matrices=False)
+    # print('starting to perform SVD')
+    # # Perform SVD
+    # U1, s1, V1 = np.linalg.svd(cacts1, full_matrices=False)
+    # U2, s2, V2 = np.linalg.svd(cacts2, full_matrices=False)
 
-    print("Fraction of variance explained by 20 singular vectors", np.sum(s1[:20])/np.sum(s1))
-    print("Fraction of variance explained by 50 singular vectors", np.sum(s1[:50])/np.sum(s1))
-    print("Fraction of variance explained by 100 singular vectors", np.sum(s1[:100])/np.sum(s1))
+    # print("Fraction of variance explained by 20 singular vectors", np.sum(s1[:20])/np.sum(s1))
+    # print("Fraction of variance explained by 50 singular vectors", np.sum(s1[:50])/np.sum(s1))
+    # print("Fraction of variance explained by 100 singular vectors", np.sum(s1[:100])/np.sum(s1))
 
-    svacts1 = np.dot(s1[:20]*np.eye(20), V1[:20])
-    # can also compute as svacts1 = np.dot(U1.T[:20], cacts1)
-    svacts2 = np.dot(s2[:20]*np.eye(20), V2[:20])
-    # can also compute as svacts1 = np.dot(U2.T[:20], cacts2)
-    print('SVD done')
+    # svacts1 = np.dot(s1[:20]*np.eye(20), V1[:20])
+    # # can also compute as svacts1 = np.dot(U1.T[:20], cacts1)
+    # svacts2 = np.dot(s2[:20]*np.eye(20), V2[:20])
+    # # can also compute as svacts1 = np.dot(U2.T[:20], cacts2)
+    # print('SVD done')
+
+    svacts1, svacts2 = acts1, acts2
 
     print('starting to perform CCA')
     svcca_results = cca_core.get_cca_similarity(svacts1, svacts2, epsilon=1e-10, verbose=False)
     print("result", np.mean(svcca_results["cca_coef1"]))
 
-    plt.plot(svcca_results["cca_coef1"], lw=2.0, label="MNIST")
-    plt.xlabel("Sorted CCA Correlation Coeff Idx")
-    plt.ylabel("CCA Correlation Coefficient Value")
-    plt.legend(loc="best")
-    plt.grid()
+    # plt.plot(svcca_results["cca_coef1"], lw=2.0, label="MNIST")
+    # plt.xlabel("Sorted CCA Correlation Coeff Idx")
+    # plt.ylabel("CCA Correlation Coefficient Value")
+    # plt.legend(loc="best")
+    # plt.grid()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="data")
