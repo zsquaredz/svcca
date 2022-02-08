939a587a8efd3e08985666d42903d396cc9633b4
zsquaredz <zhaozheng1997@hotmail.com>
2022-02-08 11:36:15 +0000

    run exp

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ MODEL_CAT2=Books
 DATA_CATEGORY2=Books
 seed2=1
 
-layer=1
+layer=2
 
 epoch=0
 echo "currently doing epoch ${epoch}, seed-${seed1}-Model-${MODEL_CAT1}-layer-${layer}, and seed-${seed2}-Model-${MODEL_CAT2}-layer-${layer}"
