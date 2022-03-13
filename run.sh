f42df970bc3b5f06d805f9a7503ec3c8f536225a
zsquaredz <zhaozheng1997@hotmail.com>
2022-03-13 03:13:08 +0000

    run exp

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ conda activate cdt
 cd /disk/ocean/zheng/svcca/
 
 
-EXP_NAME=10_data
+EXP_NAME=50_data
 
 MODEL_CAT1=top5
 DATA_CATEGORY1=Books
@@ -24,7 +24,7 @@ do
     --data_dir1 /disk/ocean/zheng/summarization_svcca/out/activations/amazon_reviews/seed${seed1}/${EXP_NAME}/${MODEL_CAT1}/epoch${epoch}/${DATA_CATEGORY1}_layer_${layer}_hidden_state.npy \
     --data_dir2 /disk/ocean/zheng/summarization_svcca/out/activations/amazon_reviews/seed${seed2}/${EXP_NAME}/${MODEL_CAT2}/epoch${epoch}/${DATA_CATEGORY2}_layer_${layer}_hidden_state.npy
 
-  for epoch in {1..502..10}
+  for epoch in {1..202..10}
   do
     echo "currently doing epoch ${epoch}, seed-${seed1}-Model-${MODEL_CAT1}-layer-${layer}, and seed-${seed2}-Model-${MODEL_CAT2}-layer-${layer}"
     python analysis.py \
