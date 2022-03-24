4b613ccd3c7361e08116c68c40eabc236e12a26f
zsquaredz <zhaozheng1997@hotmail.com>
2022-03-24 04:08:28 +0000

    run exp

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ cd /disk/ocean/zheng/svcca/
 EXP_NAME=100_data
 
 MODEL_CAT1=top5
-DATA_CATEGORY1=Home_and_Kitchen
+DATA_CATEGORY1=Movies_and_TV
 seed1=1
 
-MODEL_CAT2=Home_and_Kitchen
-DATA_CATEGORY2=Home_and_Kitchen
+MODEL_CAT2=Movies_and_TV
+DATA_CATEGORY2=Movies_and_TV
 seed2=1
 
 SVD_DIM=350
