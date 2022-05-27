a3501e417d92614e03d6bb685e907cda8889c49b
zsquaredz <zhaozheng1997@hotmail.com>
2022-05-27 03:49:17 +0100

    added script for dynamic control

--- run_dynamic_control.sh
+++ run_dynamic_control.sh
@@ -82,15 +82,15 @@ do
     #layer=8
     for layer in {0,12}
     do
-      epoch1=0
-      epoch2=${epoch_end}
-      echo "currently doing ${EXP_NAME1} epoch ${epoch1}, ${EXP_NAME2} epoch ${epoch2}, seed-${seed1}-Model-${MODEL_CAT1}-layer-${layer}, and seed-${seed2}-Model-${MODEL_CAT2}-layer-${layer}"
-      python analysis.py \
-        --data_dir1 /disk/ocean/zheng/summarization_svcca/out/activations/amazon_reviews/seed${seed1}/${EXP_NAME1}/${MODEL_CAT1}/epoch${epoch1}/${DATA_CATEGORY1}_layer_${layer}_hidden_state.npy \
-        --data_dir2 /disk/ocean/zheng/summarization_svcca/out/activations/amazon_reviews/seed${seed2}/${EXP_NAME2}/${MODEL_CAT2}/epoch${epoch2}/${DATA_CATEGORY2}_layer_${layer}_hidden_state.npy \
-        --do_svcca \
-        --svd_dim1 $SVD_DIM \
-        --svd_dim2 $SVD_DIM
+      # epoch1=0
+      # epoch2=${epoch_end}
+      # echo "currently doing ${EXP_NAME1} epoch ${epoch1}, ${EXP_NAME2} epoch ${epoch2}, seed-${seed1}-Model-${MODEL_CAT1}-layer-${layer}, and seed-${seed2}-Model-${MODEL_CAT2}-layer-${layer}"
+      # python analysis.py \
+      #   --data_dir1 /disk/ocean/zheng/summarization_svcca/out/activations/amazon_reviews/seed${seed1}/${EXP_NAME1}/${MODEL_CAT1}/epoch${epoch1}/${DATA_CATEGORY1}_layer_${layer}_hidden_state.npy \
+      #   --data_dir2 /disk/ocean/zheng/summarization_svcca/out/activations/amazon_reviews/seed${seed2}/${EXP_NAME2}/${MODEL_CAT2}/epoch${epoch2}/${DATA_CATEGORY2}_layer_${layer}_hidden_state.npy \
+      #   --do_svcca \
+      #   --svd_dim1 $SVD_DIM \
+      #   --svd_dim2 $SVD_DIM
 
       for ((epoch=1;epoch<=${epoch_end};epoch+=10))
       do
