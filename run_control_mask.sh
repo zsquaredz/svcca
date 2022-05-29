1fbfea8e53c84c991a1a83acbdc5e27e9d744a09
zsquaredz <zhaozheng1997@hotmail.com>
2022-05-29 05:00:16 +0100

    run exp

--- run_control_mask.sh
+++ run_control_mask.sh
@@ -11,7 +11,7 @@ cd /disk/ocean/zheng/svcca/
 
 # m=10
 # d=10  
-mask_type=freq # can be either general/specific freq/infreq for the mask file
+mask_type=infreq # can be either general/specific freq/infreq for the mask file
 for m in {10,25,50,75,100}
 do
   for d in {10,50,100,200}
