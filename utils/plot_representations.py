2329fd290b1a6b1a02dac7e7f9a19137fd20f0a4
zsquaredz <zhaozheng1997@hotmail.com>
2022-06-16 03:49:21 +0100

    plot embedding layer representation

--- utils/plot_representations.py
+++ utils/plot_representations.py
@@ -75,6 +75,116 @@ def plot_embedding_weights():
 
         plt.clf()
 
+def plot_five_embedding_weights():
+    plt.figure(dpi=600)
+    exp_names1 = ['10_model_100_data','25_model_100_data','50_model_100_data','75_model_100_data','100_model_100_data']
+    exp_names2 = ['new_control_10_model_100_data','new_control_25_model_100_data','new_control_50_model_100_data','new_control_75_model_100_data','new_control_100_model_100_data']
+    exp_names3 = ['new_control_10_model_100_data','new_control_25_model_100_data','new_control_50_model_100_data','new_control_75_model_100_data','new_control_100_model_100_data']
+    exp_names4 = ['new_control_10_model_100_data','new_control_25_model_100_data','new_control_50_model_100_data','new_control_75_model_100_data','new_control_100_model_100_data']
+    exp_names5 = ['new_control_10_model_100_data','new_control_25_model_100_data','new_control_50_model_100_data','new_control_75_model_100_data','new_control_100_model_100_data']
+    exp_names6 = ['new_control_10_model_100_data','new_control_25_model_100_data','new_control_50_model_100_data','new_control_75_model_100_data','new_control_100_model_100_data']
+    epoch1s = ['501','501','501','201','131'] # general
+    epoch2s = ['501','501','501','251','181'] # books
+    epoch3s = ['501','501','501','501','501'] # clothing
+    epoch4s = ['501','501','501','251','151'] # electronics
+    epoch5s = ['501','501','501','251','151'] # home
+    epoch6s = ['501','501','501','251','181'] # movie
+    for i in range(5):
+        EXP_NAME1=exp_names1[i]
+        MODEL_CAT1='top5'
+        epoch1=epoch1s[i]
+
+        EXP_NAME2=exp_names2[i]
+        MODEL_CAT2='Books'
+        epoch2=epoch2s[i]
+
+        EXP_NAME3=exp_names3[i]
+        MODEL_CAT3='Clothing_Shoes_and_Jewelry'
+        epoch3=epoch3s[i]
+
+        EXP_NAME4=exp_names4[i]
+        MODEL_CAT4='Electronics'
+        epoch4=epoch4s[i]
+
+        EXP_NAME5=exp_names5[i]
+        MODEL_CAT5='Home_and_Kitchen'
+        epoch5=epoch5s[i]
+
+        EXP_NAME6=exp_names6[i]
+        MODEL_CAT6='Movies_and_TV'
+        epoch6=epoch6s[i]
+
+        model_path1 = f'/disk/ocean/zheng/summarization_svcca/checkpoints/bert_base_uncased/amazon_reviews/seed1/{EXP_NAME1}/{MODEL_CAT1}-mlm/epoch{epoch1}'
+        model_path2 = f'/disk/ocean/zheng/summarization_svcca/checkpoints/bert_base_uncased/amazon_reviews/seed1/{EXP_NAME2}/{MODEL_CAT2}-mlm/epoch{epoch2}'
+        model_path3 = f'/disk/ocean/zheng/summarization_svcca/checkpoints/bert_base_uncased/amazon_reviews/seed1/{EXP_NAME3}/{MODEL_CAT3}-mlm/epoch{epoch3}'
+        model_path4 = f'/disk/ocean/zheng/summarization_svcca/checkpoints/bert_base_uncased/amazon_reviews/seed1/{EXP_NAME4}/{MODEL_CAT4}-mlm/epoch{epoch4}'
+        model_path5 = f'/disk/ocean/zheng/summarization_svcca/checkpoints/bert_base_uncased/amazon_reviews/seed1/{EXP_NAME5}/{MODEL_CAT5}-mlm/epoch{epoch5}'
+        model_path6 = f'/disk/ocean/zheng/summarization_svcca/checkpoints/bert_base_uncased/amazon_reviews/seed1/{EXP_NAME6}/{MODEL_CAT6}-mlm/epoch{epoch6}'
+        model1 = AutoModelForMaskedLM.from_pretrained(model_path1)
+        embeddings_1 = model1.bert.embeddings.word_embeddings.weight.data
+        embedding1_numpy = np.array(embeddings_1)
+        model2 = AutoModelForMaskedLM.from_pretrained(model_path2)
+        embeddings_2 = model2.bert.embeddings.word_embeddings.weight.data
+        embedding2_numpy = np.array(embeddings_2)
+        model3 = AutoModelForMaskedLM.from_pretrained(model_path3)
+        embeddings_3 = model3.bert.embeddings.word_embeddings.weight.data
+        embedding3_numpy = np.array(embeddings_3)
+        model4 = AutoModelForMaskedLM.from_pretrained(model_path4)
+        embeddings_4 = model4.bert.embeddings.word_embeddings.weight.data
+        embedding4_numpy = np.array(embeddings_4)
+        model5 = AutoModelForMaskedLM.from_pretrained(model_path5)
+        embeddings_5 = model5.bert.embeddings.word_embeddings.weight.data
+        embedding5_numpy = np.array(embeddings_5)
+        model6 = AutoModelForMaskedLM.from_pretrained(model_path6)
+        embeddings_6 = model6.bert.embeddings.word_embeddings.weight.data
+        embedding6_numpy = np.array(embeddings_6)
+        print(embedding1_numpy.shape)
+
+        X = np.concatenate((embedding1_numpy,embedding2_numpy,embedding3_numpy,embedding4_numpy,embedding5_numpy,embedding6_numpy),axis=0)
+
+        X_3d = PCA(n_components=2).fit_transform(X)
+        print(X_3d.shape)
+        data = {}
+        data["general"] = X_3d[:30522]
+        data["book"] = X_3d[30522:30522*2]
+        data["clothing"] = X_3d[30522*2:30522*3]
+        data["electronics"] = X_3d[30522*3:30522*4]
+        data["home"] = X_3d[30522*4:30522*5]
+        data["movie"] = X_3d[30522*5:30522*6]
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for label, marker, color in zip(['general', 'book', 'clothing', 'electronics', 'home', 'movie'], ['3', (5,2), '+', 'x', '1', '2'], ["blue", "red", 'green', 'cyan', 'yellow', 'magenta']):
+            X_temp = data[label]
+            ax.scatter(x=X_temp[:, 0], y=X_temp[:, 1],
+                    label=label,
+                    marker=marker,
+                    color=color,
+                    alpha=0.5)
+        if i==4:
+            legend = ax.legend()
+            h, l = ax.get_legend_handles_labels()
+            l = [l[0], l[1]]
+            h = [h[0], h[1]]
+            legend = ax.legend(h,
+                            l,
+                            loc='upper right',
+                            fontsize=17.5,
+                            framealpha=0.6,
+                            markerscale=2)
+            for lh in legend.legendHandles:
+                lh.set_alpha(1)
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        # ax.axis('off')
+        fig.savefig("trial_all"+str(i)+".pdf",
+                    format='pdf',
+                    bbox_inches='tight',
+                    dpi=1200,
+                    transparent=True)
+
+        plt.clf()
+
 def plot_embedding_layer_representation():
     random.seed(30)
     indices = (random.sample(range(0,430923),k=2500))
@@ -278,6 +388,7 @@ def plot_final_layer_representation():
 
 if __name__ == '__main__':
     # plot_embedding_weights()
-    plot_embedding_layer_representation()
+    # plot_embedding_layer_representation()
+    plot_five_embedding_weights()
     # plot_final_layer_weights()
     # plot_final_layer_representation()
